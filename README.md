7e16892dfec3499958d9f1cad41cbab9a9db9887
Luiz Celeghin <luiz.celeguim@gmail.com>
2024-10-27 12:14:39 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Terraform code 
+
+## Maintain VPC & EKS with Terraform
+
+## Tools required
+Terraform v1.5.7
+
+### Steps
+* terraform init
+* terraform fmt -check
+* terraform validate
+* terraform plan -out planfile
+* terraform apply -auto-approve -input=false -parallelism=1 planfile
+
+####
+#####
---
+++ terraform/terraform.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.73.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6.3"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.6"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.5"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.33.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "jvminfo-terraform-state"
+    key    = "terraform.tfstate"
+    region = "us-east-2"
+  }
+
+  required_version = "~> 1.5.7"
+}
